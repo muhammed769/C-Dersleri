dfc1e4ea5053b771610a7b36f020fc00a86075d2
muhammed769 <70298773+muhammed769@users.noreply.github.com>
2021-11-14 00:48:53 +0300

    Add files via upload

---
+++ HashTableGEnelKullanim/HashTAbleOdev/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S6.D4.HashTableOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region HashTable ÖDEV
+
+            // HashTable içerisine ENG-TR olarak data ekle.
+
+            // Yeni kayıt eklemek istiyor musunuz E/H
+            // E: Yeni kayıt ekleme işlemi devam etsin.
+            // H: Tüm listeyi yazdırın.
+            // Var olan bir key degeri ekliyor ise kullanıcıya bu deger daha önceden sistemimizde bulunmaktadır yazsın.
+
+            Hashtable Dil = new Hashtable();
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Eklemek istediginiz dil koleksiyonumuz için  deger  giriniz :");
+
+                Console.Write("EN :");
+                string eng = Console.ReadLine();
+
+
+                bool kontrol = Dil.Contains(eng);
+
+                if (kontrol)
+                {
+                    Console.WriteLine("Eklemek istediginiz deger {0} dil içerisinde bulunmaktadır.{1} degerin Türkçe karşılıgıdır.", eng, Dil[eng].ToString());
+
+                }
+                else
+                {
+                    Console.WriteLine("{0}  ingilizce degerinin türkçe karşılıgını yazınız: ",eng);
+                    string tr = Console.ReadLine();
+                    Dil.Add(eng, tr);
+                    Console.WriteLine("Deger ekleme işlemi başarılı.");
+                }
+                Console.WriteLine("Yeni Deger eklemek istiyor musunuz : (E/H)");
+
+                
+ } while (Console.ReadLine().ToUpper() !="H");
+
+             // 1.YÖNTEM :
+
+            foreach (var item in Dil.Keys)
+
+            {
+                Console.WriteLine(" ENG : {0} = TR :{1} ", item, Dil[item]);
+
+            }
+
+            // 2.YÖNTEM :
+
+            foreach (DictionaryEntry item in Dil)
+            
+
+           {
+                //Console.WriteLine(item.GetType().Name);
+                Console.WriteLine(" ENG : {0} = TR :{1} ", item.Key, item.Value);
+            }
+            
+
+
+
+
+            #endregion
+
+
+
+
+        }
+    }
+}
---
+++ HashTableGEnelKullanim/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S6.D3.HashTableGenelKullanim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable H1 = new Hashtable();
+
+
+            #region Yeni Deger Ekleme
+
+            // H1.Add(object key, object value)  key=key degerleri Liste içinde dataya ulaşmak için kullandıgımız anahtar kelimelerdir ve 1 defa kullanılır.
+
+            H1.Add("Car", "Araba");
+            H1.Add("Home", "Ev");
+
+            H1.Add("Cars", "Araba");
+          //   H1.Add("Cars", "Araba"); // BU kısımda hata alırız çünkü KEY Anahtar kelimesi(Yani burda Cars olan kelime) 2 kere kullanılmaz.
+                                     //SADECE VE SADECE KEY ANAHTAR KELİMESİ 1 DEFA KULLANILIR.
+
+
+            #endregion
+
+
+            #region Yardımcı Metotlar
+            bool kontrol1 = H1.Contains("Car"); // Car anahtar kelimesi(YANİ key) H1 listesinde mi  demektir bu kod. 
+            bool kontrol2 = H1.Contains("Pencil");
+
+            bool kontrol3 = H1.ContainsKey("Cars"); // ContainsKey Contain ile birebir  aynı işlevi görür.
+            bool kontrol4 = H1.ContainsValue("Araba");  // Value degeri H1 listemizde mevcut mu demektir bu kod.
+
+
+
+            //Peki ben koleksiyon içinde bir düzenleme yani bir degeri degiştirmek istersem şunu yapmam gerekir :
+            H1["Home"] = "Yazlık";
+
+
+
+
+            H1.Clear();  //Koleksiyon içindeki tüm datayı siler.
+
+            int koleksiyonicindekiToplamDeger = H1.Count;
+
+            H1.Remove("Cars"); // H1 koleksiyonumuz içindeki Cars keyini siler.
+
+            #endregion
+
+
+        }
+    }
+}
