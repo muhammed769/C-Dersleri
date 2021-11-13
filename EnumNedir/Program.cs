0635327a20f6cd253449e3f0d6e11612ae50d178
muhammed769 <70298773+muhammed769@users.noreply.github.com>
2021-11-14 00:56:37 +0300

    Add files via upload

---
+++ EnumNedir/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S17.D1.EnumNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+
+            // SORU : Enum neden kullanılır ? CEVAP : Numaralandırmalar (enumerations) kod içerisinde sayısal karşılaştırma veya işlem gerektiren yerlerde yazılımcı için daha okunabilirlik sunan, kod karmaşasını azaltan yardımcı bir yapıdır.
+
+            // Kod yazarken sade ve temiz bir şekilde yazmalıyız.Dolayısıyla bir kurgumuzun olması lazım.
+
+
+
+            Musteri M1 = new Musteri();
+
+            M1.id = 1;
+            M1.musteriNumara = "MSN001";
+            M1.isim = "MİKA";
+            M1.Soyisim = "cevizli";
+            M1.emailAdres = "mikacevizli.com";
+
+            
+          
+              sanalDatabase SB = new sanalDatabase();
+              musteriReturnValue musteriKayitSonuc= SB.musteriYeniKayit(M1);
+                                                   
+                                                            
+                if (musteriKayitSonuc==musteriReturnValue.kayitBasarili)
+
+                { 
+
+                }
+            
+
+                
+        }
+    }
+}
---
+++ EnumNedir/musteriReturnValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S17.D1.EnumNedir
+{
+    public enum musteriReturnValue // Numaralandırmalar (enumerations) kod içerisinde sayısal karşılaştırma veya işlem gerektiren yerlerde yazılımcı için daha okunabilirlik sunan, kod karmaşasını azaltan yardımcı bir yapıdır.
+
+    {
+        kayitBasarili,
+        kayitBasarisiz,
+        varolanMusteri,
+        parametreHatasi,
+        çalismaZamanHatasi
+
+    }
+}
