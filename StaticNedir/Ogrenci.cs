d629504787ffb5cb1e704946a43efa81511868e5
muhammed769 <70298773+muhammed769@users.noreply.github.com>
2021-11-14 00:53:10 +0300

    Add files via upload

---
+++ StaticNedir/Ogrenci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S10.D1.StaticNedir
+{
+    public  class Ogrenci // internal olarakta ayarlayabilirdik.
+
+    {
+
+        public string Isim { get; set; }  // Prop yazıp 2 kere TAB'a basarsan bu kullanım otomatik olarak gelir.
+
+        public string SoyIsim { get; set; }
+        public  string EmailAdres { get; set; }
+
+
+        public void Test1()
+        {
+            Console.WriteLine("Bu metot Nesne örneği alındıktan sonra çalışan metottur.");
+
+            Test2();   //  ÖNEMLİ 1 : Static olmayan bir metotun içinde static olan bir başka metodu ÇAĞRILABİLİR ve  işlem yapabilirim.
+        }
+
+        public static void Test2()
+        {
+            Console.WriteLine("Bu metot Nesne örneği ALINMADAN kullanılabilir.");
+
+            // ÖNEMLİ 2 : Static olan bir metotta ise static olmayan bir başka metot ÇAĞRILAMAZ.Yani static bir metotta SADECE static metotlar çağrılır.
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ StaticNedir/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S10.D1.StaticNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Ogrenci O1;  // Stack bölgede pointer'ımız (İşaretimiz) oluştu.
+
+                        // Stack bölgede pointer(İşaret) oluştuğu zaman  static olan bir yapıcı metot çalıştı ve yine class içerisinde tanımlı olan static field,metot static dediğimiz bölgede oluşturuldu.
+
+
+            O1 = new Ogrenci();  // Stack bölgede oluşan pointer kendisini heap bölgede static OLMAYAN tanımlarını tamamladı.
+
+            O1.Test1();          // Nesne örneği ALINDIKTAN SONRA Test1 adındaki metoduma ulaşabildim.
+
+            Ogrenci.Test2();     // Nesne örneği ALMADAN Test2 adındaki metoda ulaştım.
+
+
+            
+            /* Static Kavranu gerçek hayatta kullanıldığı yerler : 
+            
+             
+             * 1 - Helper.cs => Kontroller 
+             
+             
+             * 2 - Oluşturmuş olduğumuz nesnemizin bağımlılığı farklı class'lara bağlı olduğu için (Yani proje içinde özel olduğu için) nesne içerisinde static metot tanımları yapılır.
+
+
+             * 3 - Class içerisinde bir field (static) tanımı yapar, ilgili field içerisine bir değer ataması yaparız.Daha sonra uygulama içerisinde N farklı yerde oluşturmuş olduğumuz static field'ımıza ulasir değerimizi okuruz.
+
+            * 4 -  [ Uygulama ] = > [ozelHelper] = > [DLL]
+
+            */
+
+        }
+    }
+}
