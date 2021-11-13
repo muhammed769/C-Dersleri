f6e9da1e8339d08fd5101e95d052a3aa47795fe4
muhammed769 <70298773+muhammed769@users.noreply.github.com>
2021-11-14 00:56:57 +0300

    Add files via upload

---
+++ GenericOncesiIslemler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S18.D1.GenericOncesiIslemler
+{
+    class Program
+    {
+        static void Main(string[] args)
+       
+        {
+
+            /*
+              ArrayList liste = new ArrayList();       object 'den değer taşıyon bir koleksiyondu yani her tipi(int,string.vb.)  buraya ekleyebiliyodum.Ama benim isteğim şuydu :
+
+                liste.Add();                           oluşturmuş oldugum koleksiyona sadece int girilebilsin veya sadece string girilebilsin.İşte bu gibi işlemlerin çözümü için bizim bu ArrayList'i başka bir class içerisinde saklayıp  içerisine
+
+                                                       datayı metotlar ile  giriyoduk.
+
+
+            */
+
+
+            sanalDatabase sdatabase = new sanalDatabase();
+            sdatabase.yeniKayit(1);
+            sdatabase.yeniKayit(2);
+
+        }
+    }
+}
---
+++ GenericOncesiIslemler/sanalDatabase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S18.D1.GenericOncesiIslemler
+{
+    public class sanalDatabase
+   
+    {
+
+        private ArrayList listem;
+        public sanalDatabase()
+        {
+            listem = new ArrayList();
+        }
+
+        public void yeniKayit(int data)
+        {
+            listem.Add(data);
+                               /* İçeride saklamış oldugum ArrayList'ime(koleksiyonuma) istemiş olduugum  zorunlu koşmuş oldugum veri tipini (int 'ı aldık.) alabildim.
+
+                               * Artık ArrayList'im yeniKayıt metodunu kullanarak  int veri tiplerini  alabilir hale gelmiş oldu.
+
+                                 İşte bu kullanımı yapıyoduk AMA GENERİC GELMEDEN ÖNCE ÇÜNKÜ GENERİC MİMARİ GELDİGİNDE  GENERİC MİMARİYLE BERABER GENERİC KOLEKSİYONLAR GELDİ.
+
+                               * Class'ın , Metodun, interface'in  GENERİC DAVRANIŞLARI GELİŞTİ.YANİ BİR DAVRANIŞ ŞEKLİ GELDİ GENERİC DAVRANIŞ DİYE.
+
+                                  VE BUNLAR BİZE Class'ta , Metotta, interface'de,, listelerde ESNEKLİK SUNDU, İŞTE BU BÖLÜMDE BUNLARDAN DETAYLI OLARAK BAHSEDİCEZ...
+
+           */
+
+        }
+
+
+    }
+}
