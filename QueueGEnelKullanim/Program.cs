09c3ae416b3ee16637981e365b86d6d5f78065fa
muhammed769 <70298773+muhammed769@users.noreply.github.com>
2021-11-14 00:49:50 +0300

    Add files via upload

---
+++ QueueGEnelKullanim/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S6.D7.QueueGenelKullanim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue Q1 = new Queue();
+
+            Q1.Enqueue("Bir");         //Koleksiyona objeyi ekler. EKLEDİGİMİZ sırada çıktı verir.
+            Q1.Enqueue("iki");
+            Q1.Enqueue("üç");
+            Q1.Enqueue("dört");
+
+
+            object O1 = Q1.Peek();   //Degerleri yansıtır.
+            object O2 = Q1.Dequeue(); // İLK degerden silmeye başlar.
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        
+    }
+}
