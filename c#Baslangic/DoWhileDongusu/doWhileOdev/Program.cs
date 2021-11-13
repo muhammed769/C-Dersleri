1f5421c515b415550b044d7ae7bd37a6cbfe2c7d
muhammed769 <70298773+muhammed769@users.noreply.github.com>
2021-11-14 00:58:39 +0300

    Add files via upload

---
+++ c#Baslangic/DoWhileDongusu/doWhileOdev/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S4.D5.doWhileOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Odev 1 : Kullanıcıyı do while içerisine alın ve kullanıcı adı ve password sorun. Kullanıcı adı : Demo Şifre : Demo eğer kullanıcı bu değerleri bilirse döngüden çıkın ve başarılı yazın. Eğer yanlış giriş yapıyorsa do while ile ilgili değerleri sormaya devam edin.
+
+            #endregion
+
+            #region Odev 2 : Kullanıcıdan 1 ile X arasında bir sayı girmesini isteyin. Daha sonra sistem kullanıcının girmiş olduğu 1 ile X değerleri arasında bir sayı tahmini yapsın. Daha sonra kullanıcu sistemin tahmin etmiş olduğu sayıyı bulmaya çalışssın. Bulamaz ise yeniden yeniden sorsun. Ne zaman kullanıcı ilgili değeri bulursa Y kadar denediniz ve sonuca ulaştınız desin. 
+            #endregion
+        }
+    }
+}
